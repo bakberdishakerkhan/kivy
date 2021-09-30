77a73b0c59194878b0a0f9b4b21b5559bd13f3e8
bakberdishakerkhan <88204773+bakberdishakerkhan@users.noreply.github.com>
2021-09-30 07:06:47 +0300

    Update new.py

--- new.py
+++ new.py
@@ -85,3 +85,39 @@ class PulseScr2(Screen):
         line2 = BoxLayout(size_hint=(0.8, None), height='30sp')
         lbl_result2 = Label(text='Результат после отдыха:', halign='right')
         self.in_result2 = TextInput(text='0', multiline=False)
+        line2.add_widget(lbl_result2)
+        line2.add_widget(self.in_result2)
+        self.btn = Button(text='Завершить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn.on_press = self.next
+        outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(line1)
+        outer.add_widget(line2)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
+    def next(self):
+        global p2, p3
+        p2 = int(self.in_result1.text)
+        p3 = int(self.in_result2.text)
+        self.manager.current = 'result'
+class Result(Screen):
+    def __init__(slef, **kwargs):
+        self.outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        self.instr = Label(text='')
+        self.outer.add_widget(self.instr)
+        self.add_widget(self.outer)
+        self.on_enter = self.before
+    def before(self):
+        global name
+        self.instr.text = name + '\n' + test(p1, p2, p3, age)
+class HeartCheck(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(InstScr(name='instr'))
+        sm.add_widget(PulseScr(name='pulsel'))
+        sm.add_widget(CheckSits(name='sits'))
+        sm.add_widget(PulseScr2(name='pulse2'))
+        sm.add_widget(Result(name='result'))
+        return sm
+app = HeartCheck()
+app.run()
