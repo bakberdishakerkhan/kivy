7d626fe2dc18e9b3c52d1c548f99ffb223b66b42
bakberdishakerkhan <88204773+bakberdishakerkhan@users.noreply.github.com>
2021-10-09 11:39:29 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -17,10 +17,10 @@ p1, p2, p3 = 0, 0, 0
 def check_int(str_num):
 
     try:
-        return int(strn)
+        return int(str_num)
     except:
         return False
-        ``
+      
 class InstScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -99,9 +99,16 @@ class CheckSits(Screen):
         self.manager.current = 'pulse2'
 class PulseScr2(Screen):
     def __init__(self, **kwargs):
+        self.next_screen = False
+
+        self.stage = 0
         super().__init__(**kwargs)
         instr = Label(text=txt_test3)
         line1 = BoxLayout(size_hint=(0.8, None), height='30sp')
+        self.lbl_sec = Seconds(15)
+        self,lbl_sec.bind(done=self.sec_finished)
+        self.lbl1 = Label(text='Считайте пульс')
+
         lbl_result1 = Label(text='Результат:', halign='right')
         self.in_result1 = TextInput(text='0', multiline=False)
         line1.add_widget(lbl_result1)
@@ -109,12 +116,17 @@ class PulseScr2(Screen):
         line2 = BoxLayout(size_hint=(0.8, None), height='30sp')
         lbl_result2 = Label(text='Результат после отдыха:', halign='right')
         self.in_result2 = TextInput(text='0', multiline=False)
+
+        self.in_result1.set_disabled(True)
+        self.in_result2.set_disabled(True)
         line2.add_widget(lbl_result2)
         line2.add_widget(self.in_result2)
         self.btn = Button(text='Завершить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
         self.btn.on_press = self.next
         outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
         outer.add_widget(instr)
+        outer.add_widget(self.lbl1)
+        outer.add_widget(self.lbl_sec)
         outer.add_widget(line1)
         outer.add_widget(line2)
         outer.add_widget(self.btn)
@@ -137,12 +149,25 @@ class PulseScr2(Screen):
                 self.btn.text = 'Завершить'
                 self.next_screen = True
     def next(self):
-        global p2, p3
-        p2 = int(self.in_result1.text)
-        p3 = int(self.in_result2.text)
-        self.manager.current = 'result'
+        if not self.next_screen:
+            self.btn.set_disabled(True)
+            self.lbl_sec.start()
+        else:
+            global p2, p3
+            p2 = check_int(self.in_result1.text)
+            p3 = check_int(self.in_result2.text)
+            if p2 == False:
+                p2 = 0
+                self.in_result1.text = str(p2)
+            elif p3 == False:
+                p3 = 0
+                self.in_result2.text = str(p3)
+            else:
+                self.manager.current = 'result'
+
 class Result(Screen):
     def __init__(slef, **kwargs):
+        super().__init__(**kwargs)
         self.outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
         self.instr = Label(text='')
         self.outer.add_widget(self.instr)
