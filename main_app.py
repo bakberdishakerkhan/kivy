a4ddba3dd62be0ea9f6c9fed73c5ad19c80976e3
bakberdishakerkhan <88204773+bakberdishakerkhan@users.noreply.github.com>
2021-10-09 19:17:06 +0600

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -7,10 +7,16 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput 
 from kivy.core.window import Window 
 from kivy.uix.scrollview import ScrollView 
-from instruction import *
+from instructions import *
 from ruffier import test
 
 from seconds import Seconds
+from sits import Sits
+from runner import Runner
+
+Window.clearcolor = (.87, 0.54, 0.8, 0.3)
+btn_color = (0.98, 0.31, 0.8, 1)
+
 age = 7
 name = ''
 p1, p2, p3 = 0, 0, 0
@@ -24,25 +30,33 @@ def check_int(str_num):
 class InstScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
         instr = Label(text=txt_instruction)
+
         lbl1 = Label(text='Введите имя:', halign='right')
         self.in_name = TextInput(multiline=False)
         lbl2 = Label(text="Введите возраст:", halign='right')
+
         self.in_age = TextInput(text='7', multiline=False)
         self.btn = Button(text='Начать', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn.background_color = btn_color
         self.btn.on_press = self.next
+
         line1 = BoxLayout(size_hint=(0.8, None), height='30sp')
         line2 = BoxLayout(size_hint=(0.8, None), height='30sp')
         line1.add_widget(lbl1)
         line1.add_widget(self.in_name)
         line2.add_widget(lbl2)
         line2.add_widget(self.in_age)
+
         outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
         outer.add_widget(instr)
         outer.add_widget(line1)
         outer.add_widget(line2)
         outer.add_widget(self.btn)
+
         self.add_widget(outer)
+
     def next(self):
         name = self.in_name.text
         age = check_int(self.in_age.text)
@@ -56,22 +70,38 @@ class InstScr(Screen):
 class PulseScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.next_screen = False
 
         instr = Label(text=txt_test1)
+        self.lbl_sec = Seconds(15)
+        self.lbl_sec.bind(done=self.sec_finished)
 
-        line = BoxLayout(size_hint=(0.8, None), height='30sp-')
+        line = BoxLayout(size_hint=(0.8, None), height='30sp')
         lbl_result = Label(text='Введите результат:', halign='right')
         self.in_result = TextInput(text='0', multiline=False)
+        self.in_result.set_disabled(True)
 
         line.add_widget(lbl_result)
         line.add_widget(self.in_result)
-        self.btn = Button(text='Продолжить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+
+        self.btn = Button(text='Начать', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn.background_color = btn_color
         self.btn.on_press = self.next
+
         outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
         outer.add_widget(instr)
+        outer.add_widget(self.lbl_sec)
         outer.add_widget(line)
         outer.add_widget(self.btn)
+
         self.add_widget(outer)
+        
+    def sec_finished(self, *args):
+        self.next_screen = True
+        self.in_result.set_disabled(False)
+        self.btn.set_disabled(False)
+        self.btn.text = 'Продолжить'
+        
     def next(self):
         if not self.next_screen:
             self.btn.set_disabled(True)
@@ -88,7 +118,10 @@ class PulseScr(Screen):
 class CheckSits(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        instr = Label(text=txt_sits)
+        self.next_screen = False
+        
+        instr = Label(text=txt_sits, size_hint=(0.5, 1))
+        self.lbl_sits = Sits(30)
         self.btn = Button(text='Продолжить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
         self.btn.on_press = self.next
         outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
@@ -106,7 +139,7 @@ class PulseScr2(Screen):
         instr = Label(text=txt_test3)
         line1 = BoxLayout(size_hint=(0.8, None), height='30sp')
         self.lbl_sec = Seconds(15)
-        self,lbl_sec.bind(done=self.sec_finished)
+        self.lbl_sec.bind(done=self.sec_finished)
         self.lbl1 = Label(text='Считайте пульс')
 
         lbl_result1 = Label(text='Результат:', halign='right')
@@ -166,7 +199,7 @@ class PulseScr2(Screen):
                 self.manager.current = 'result'
 
 class Result(Screen):
-    def __init__(slef, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
         self.instr = Label(text='')
