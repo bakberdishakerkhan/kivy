9a1447772235f35d3dce220ba5023d955f4d12af
bakberdishakerkhan <88204773+bakberdishakerkhan@users.noreply.github.com>
2021-10-09 11:50:47 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,7 +5,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label 
 from kivy.uix.button import Button 
 from kivy.uix.textinput import TextInput 
-from kivy.uix.window import Window 
+from kivy.core.window import Window 
 from kivy.uix.scrollview import ScrollView 
 from instruction import *
 from ruffier import test
